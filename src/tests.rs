a8d56d2873715fc73c1eaa6bdaea9c815b4cbf20
Dariia Porechna <35382599+dariolina@users.noreply.github.com>
2022-10-21 10:56:38 +0200

    Second way to compute root

--- src/tests.rs
+++ src/tests.rs
@@ -77,22 +77,50 @@ fn test_filesizes() {
     assert_eq!(scheme.get_filesize_in_bytes(), 253 * 256*1024 / 8);
 }
 #[test]
-fn test_commit_commit_bls12_381() {
-    _test_commit_commit::<Bls12_381>()
+fn test_commit_commit1_bls12_381() {
+    _test_commit_commit1::<Bls12_381>()
 }
 //tests 'commitment to commitments' i.e a polynomial where commitments are coefficients evaluated at public parameters
-fn _test_commit_commit<E: PairingEngine>() {
+fn _test_commit_commit1<E: PairingEngine>() {
     let mut rng = ark_std::rand::thread_rng();
     
     let scheme = SemiAvidPr::<E>::setup(&mut rng, 16, 8, 1024);
     let data_uncoded = scheme.generate_random_file(&mut rng);
-    let mut comm_root = E::G1Projective::zero();
+    let mut comms_list= Vec::new();
+    let mut comm_root1 = E::G1Projective::zero();
+
+    let mut comm_root2 = E::G1Projective::zero();
 
     //i iterates columns
     for i in 0..scheme.k {
         let poly_evals = Evaluations::from_vec_and_domain(data_uncoded.iter().map(|r| r[i]).collect(), scheme.domain_polycommit);
         let poly_poly = poly_evals.interpolate();
 
+        //compute commitment
+        let mut commitment = E::G1Projective::zero();
+        //compute corresponding term into commitment root
+        let mut comm_root_term1 = E::G1Projective::zero();
+        let mut comm_root_term2 = E::G1Projective::zero();
+
+
+        assert_eq!(scheme.kzg10_ck.powers_of_g.len(), poly_poly.coeffs.len()+scheme.k);
+
+        //j iterates rows
+        for j in 0..scheme.kzg10_ck.powers_of_g.len()-scheme.k {
+            commitment += scheme.kzg10_ck.powers_of_g[j].mul(poly_poly.coeffs[j]);
+            comm_root_term1 += scheme.kzg10_ck.powers_of_g[j+i].mul(poly_poly.coeffs[j]);
+            comm_root_term2 += commitment + scheme.kzg10_ck.powers_of_g[i].mul(poly_poly.coeffs[j]);
+        }
+        comms_list.push(commitment);
+        comm_root1 += comm_root_term1;
+        comm_root2 += comm_root_term2;
+        
+    }   
+    assert_ne!(
+            comm_root1,
+            comm_root2
+     );
+}
         //compute corresponding term into commitment root
         let mut comm_root_term = E::G1Projective::zero();
 
@@ -100,9 +128,11 @@ fn _test_commit_commit<E: PairingEngine>() {
 
         //j iterates rows
         for j in 0..scheme.kzg10_ck.powers_of_g.len()-scheme.k {
+            commitment += scheme.kzg10_ck.powers_of_g[j].mul(poly_poly.coeffs[j]);
             comm_root_term += scheme.kzg10_ck.powers_of_g[j+i].mul(poly_poly.coeffs[j]);
         }
-        
+        comms_list.push(commitment);
+
         comm_root += comm_root_term;
         
     }   
@@ -112,4 +142,19 @@ fn _test_commit_commit<E: PairingEngine>() {
             !comm_root.is_zero(),
             "oops"
      );
+    assert_eq!(
+        comms_list.len(),
+        scheme.k
+    );
+
+    //compute witness polynomial to some commitment
+    //let point = 5;
+    //let mut divisor=E::G1Affine::zero();
+    //divisor += scheme.kzg10_ck.powers_of_g[1];
+   //divisor-=scheme.kzg10_ck.powers_of_g[point];
+
+   // assert_eq!(
+    //    divisor,
+    //    E::G1Affine::zero()
+    //);
 }
