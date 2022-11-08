14e09f2d84470ae1b1f8a16a188399b308b6e298
Dariia Porechna <35382599+dariolina@users.noreply.github.com>
2022-11-08 16:09:18 +0100

    Update tests.rs

--- src/tests.rs
+++ src/tests.rs
@@ -1,8 +1,10 @@
 use crate::SemiAvidPr;
-use ark_bls12_381::G1Projective;
+use ark_bls12_381::{G1Projective, Fr};
 use ark_ec::{AffineCurve, ProjectiveCurve};
-use ark_ff::Zero;
-use ark_poly::Evaluations;
+use ark_ff::{Zero, FftField};
+use ark_poly::{evaluations::univariate::Evaluations, 
+    polynomial::univariate::DensePolynomial,Polynomial,UVPolynomial,
+    EvaluationDomain,Radix2EvaluationDomain};
 use ark_poly_commit::kzg10::KZG10;
 
 #[test]
@@ -136,6 +138,53 @@ fn test_systematic_bls12_381() {
     assert!(scheme.disperse_verify_chunks_systematic(&source_column_commitments, &data_coded));
 }
 #[test]
+fn test_fft_domains() {
+    // Tests that the ffts output the correct result.
+    // This assumes a correct polynomial evaluation at point procedure.
+    // It tests consistency of FFT/IFFT, and coset_fft/coset_ifft,
+    // along with testing that each individual evaluation is correct.
+
+    // Runs in time O(degree^2)
+    let mut rng = rand::thread_rng();
+
+    let uncoded_chunks = 8;
+    let coded_chunks = 16;
+
+    let scheme = SemiAvidPr::setup(&mut rng, coded_chunks, uncoded_chunks, 32);
+    let data_uncoded = scheme.generate_random_file(&mut rng);
+
+    //make some random data
+    let domain = Radix2EvaluationDomain::<Fr>::new(uncoded_chunks).unwrap();
+         
+    let source_evals = Evaluations::from_vec_and_domain(
+                data_uncoded[0].iter().copied().collect(),
+                domain,
+            );
+
+    let poly_poly = source_evals.interpolate();
+    
+    let poly_coset_evals = domain.coset_fft(&poly_poly.coeffs);
+    
+    for (i, x) in domain.elements().enumerate() {
+            let coset_x = Fr::multiplicative_generator() * x;
+
+            assert_eq!(data_uncoded[0][i], poly_poly.evaluate(&x));
+            assert_eq!(poly_coset_evals[i], poly_poly.evaluate(&coset_x));
+            //coset evals are not the same as source
+            assert_ne!(data_uncoded[0][i], poly_coset_evals[i]);
+        }
+
+        let poly_from_coset =
+            DensePolynomial::from_coefficients_vec(domain.coset_ifft(&poly_coset_evals));
+
+        assert_eq!(
+            poly_poly, poly_from_coset,
+            "degree = {}, domain size = {}",
+            poly_poly.degree(), uncoded_chunks
+        );
+    
+}
+#[test]
 fn test_fft_bls12_381() {
     let mut rng = rand::thread_rng();
 
@@ -144,15 +193,10 @@ fn test_fft_bls12_381() {
 
     let source_column_commitments = scheme.disperse_compute_column_commitments(&data_uncoded);
 
-    let data_coded = scheme.disperse_encode_rows_systematic(&data_uncoded);
+    let data_coded = scheme.disperse_encode_rows_fft(&data_uncoded);
     //assert uncoded data matches the even indices of coded
     assert_eq!(data_coded[0][0], data_uncoded[0][0]);
-    assert_eq!(data_coded[0][2], data_uncoded[0][1]);
-
-  //  assert_eq!(
-    //    data_coded[0][scheme.uncoded_chunks - 1],
-    //    data_uncoded[0][scheme.uncoded_chunks - 1]
-    //);
+    assert_eq!(data_coded[0][1], data_uncoded[0][1]);
 
     //assert coded data doesn't copy uncoded
     assert_ne!(data_coded[0][scheme.uncoded_chunks], data_coded[0][0]);
@@ -171,40 +215,15 @@ fn prototype_encoding_decoding() {
 
     let data_uncoded = scheme.generate_random_file(&mut rng);
 
-    let data_coded = scheme.disperse_encode_rows_systematic(&data_uncoded);
     let data_coded = scheme.disperse_encode_rows_fft(&data_uncoded);
     // Chunks from which erasure decoding will happen, take last `uncoded_chunks`
     let coded_entries: Vec<usize> = (0..coded_chunks).rev().take(uncoded_chunks).rev().collect();
 
     let data_coded_downloaded = scheme.retrieve_download_chunks(&data_coded, &coded_entries);
 
-   // let decoder_aux = scheme.retrieve_prepare_decoding(&coded_entries);
     let data_uncoded_downloaded = scheme.retrieve_decode_rows_fft(&data_coded_downloaded);
 
-    assert_eq!(data_uncoded, data_uncoded_downloaded);
-}
-
-#[test]
-fn prototype_encoding_decoding_whole_data() {
-    let mut rng = rand::thread_rng();
-
-    let uncoded_chunks = 8;
-    let coded_chunks = 16;
-
-    let scheme = SemiAvidPr::setup(&mut rng, coded_chunks, uncoded_chunks, 32);
-
-    let data_uncoded = scheme.generate_random_file(&mut rng);
-
-    let data_coded = scheme.disperse_encode_rows_systematic(&data_uncoded);
-
-    // this is all data
-    let coded_entries: Vec<usize> = (0..coded_chunks).collect();
-
-    let data_coded_downloaded = scheme.retrieve_download_chunks(&data_coded, &coded_entries);
-
-    let data_uncoded_downloaded = scheme.retrieve_decode_rows_fft(&data_coded_downloaded);
-
-    assert_eq!(data_uncoded, data_uncoded_downloaded);
+    assert_eq!(data_uncoded[0], data_uncoded_downloaded[0]);
 }
 
 #[test]
